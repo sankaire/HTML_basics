76a48a6dbbb131957f680b8b63d2afe95a96fb2e
sankaire <kerenketepela@gmail.com>
2021-09-25 18:11:15 +0300

    create a js folder, add functions and variables files

---
+++ js/function.js
@@ -0,0 +1,10 @@
+// function errAlert(){
+//     alert("wrong click")
+// }
+
+//arrow functions
+const btn = document.querySelector("button")
+
+btn.onclick = () =>{
+    alert("hello")
+}
---
+++ js/variables.js
@@ -0,0 +1,12 @@
+// let is same as var
+let PersonName = "Tepela";//string
+let PersonAge = 21;//number
+let isMale = true //boolean
+// PersonName = "peter"
+
+//const -> the variable declared dosen't change
+const cumpus = "Zetech"
+
+//displaying it on the web browser using a formated string
+// document.write(`${cumpus} is ${PersonAge} years old`)
+
